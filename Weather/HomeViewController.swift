8e3b3b2b8f20cde888ac5bbca814171e873a8354
Vladimir Lesnykh <v.a.lesnykh@gmail.com>
2022-04-12 15:42:47 +0300

    Init project

---
+++ Weather/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Владимир on 12.04.2022.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
