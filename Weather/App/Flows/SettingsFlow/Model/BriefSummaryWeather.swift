58c5e1db02d75459631f1657de24524e3d9387ef
Vladimir Lesnykh <v.a.lesnykh@gmail.com>
2022-05-30 16:15:20 +0300

    ref

---
+++ Weather/App/Flows/SettingsFlow/Model/BriefSummaryWeather.swift
@@ -0,0 +1,14 @@
+//
+//  BriefSummaryWeather.swift
+//  Weather
+//
+//  Created by Владимир on 30.05.2022.
+//
+
+import UIKit
+
+struct BriefSummaryWeather {
+    let city: String
+    let icon: UIImage
+    let temperature: String
+}
